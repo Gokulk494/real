0d7f71f0cf4c0ea810a757390f1a71aa0dfa2636
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2023-12-20 14:45:18 +0530

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,19 @@
+// src/main/java/eight/service/UserService.java
+
+package eight.service;
+
+import eight.model.User;
+import eight.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+}
