9d8ffb526ccebb4513ef80ad83760d30dbfae662
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2023-12-20 14:42:12 +0530

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,32 @@
+// src/main/java/eight/controller/UserController.java
+
+package eight.controller;
+
+import eight.model.User;
+import eight.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistration(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        // Redirect to login page after registration
+        return "redirect:/login";
+    }
+}
